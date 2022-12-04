8e5d71ee51f44f3032bd784290190dc81b83da78
razanalamri <razan.f96@outlook.com>
2022-12-04 16:13:43 +0400

    Another solution

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -33,25 +34,7 @@ public class Test {
         .build();
   	   HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
   	   
-  	   //Read 
-  	   
-  	    try {
-  	   
-  	    FileReader reader = new FileReader("Evalution.txt");
-
-  	    
-  	    int ch;
-  	    while ((ch = reader.read()) != -1) {
-  	        System.out.print((char) ch);
-  	    }
-
-  	    
-  	    reader.close();
-
-  	} catch (IOException ex) {
-  	    ex.printStackTrace();
-  	}
-   	 
+  	 
   	  //Write  
 
      	try {
@@ -69,56 +52,88 @@ public class Test {
      	    ex.printStackTrace();
      	}
      	
+     	
+     	
+     	
+     	 Scanner sc = new Scanner(System.in);
+         
+     	 System.out.println("Enter the word to be found");
+         String word = sc.next();
+         boolean flag = false;
+         int count = 0;
+
+         
+         Scanner sc2 = new Scanner(new FileInputStream("Evalution.txt"));
+         while(sc2.hasNextLine()) {
+            String line = sc2.nextLine();
+            System.out.println(line);
+            if(line.indexOf(word)!=-1) {
+               flag = true;
+               count = count+1;
+            }
+         }
+         if(flag) {
+            System.out.println("File contains the specified word");
+            System.out.println("Number of occurrences is: "+count);
+         } else {
+            System.out.println("File does not contain the specified word");
+         }
+      }
+   }
      	//Searching for Unique word 
-     	
-      File data=new File("C:\\Users\\user012\\eclipse-workspace\\Evaluation\\Evalution.txt");
-   	  FileReader fileR=null;
-   	  Scanner scanner1=new Scanner(System.in);
-   	  String string;
-   
-	  System.out.println("Enter word you want to search:");
-	  String SearchWord= scanner1.next();
-	  listWord.add(SearchWord);
-	  System.out.println("Enter word you want to search:");
-	  String SearchWord2= scanner1.next();
-	  listWord.add(SearchWord2);
-   	 
-   	 
-	  
-   	 try {
-   		fileR=new FileReader(data);
-		  BufferedReader bufferR=new BufferedReader(fileR);
-		  
-		  while((string=bufferR.readLine())!=null) {
-			  if(string.contains(SearchWord)) {
-				  for(String word:listWord) {
-           		   if(set.add(word)==true) {
-				  System.out.println("Yes, "+word+" is in the file");
-			  	}}}
-			
-
-			  else {
-				  System.out.println("No, "+SearchWord+" is Not in the file");
-				  System.out.println("No, "+SearchWord2+" is Not in the file");
-			 }}}
-           			  
-
-
-   		
-   	 
-   				  
-   			 
-   			     catch(FileNotFoundException ex) {
-   					  Logger.getLogger(Test.class.getName()).log(Level.SEVERE,null,ex);
-   				     }catch(IOException ex) {
-   					  Logger.getLogger(Test.class.getName()).log(Level.SEVERE,null,ex);
-   				  }
-   				  finally {
-   					  fileR.close();
-   				  }
-     	
-     	
-        
-		
-		
-}}
+//     	
+//      File data=new File("C:\\Users\\user012\\eclipse-workspace\\Evaluation\\Evalution.txt");
+//   	  FileReader fileR=null;
+//   	  Scanner scanner1=new Scanner(System.in);
+//   	  String string;
+//      String[] json=response.body().split(" ");
+//      
+//	  System.out.println("Enter word you want to search:");
+//	  String SearchWord= scanner1.next();
+//	  int count=0;
+//	  for(int i=0; i<json.length;i++) {
+//		  if(json[i]==SearchWord) {
+//			  count++;
+//		  }
+//	  }
+//
+//   	 
+//   	 
+//	  
+//   	 try {
+//   		fileR=new FileReader(data);
+//		  BufferedReader bufferR=new BufferedReader(fileR);
+//		  
+//		  while((string=bufferR.readLine())!=null) {
+//			  if(string.contains(SearchWord)) {
+//				  for(String word:listWord) {
+//           		   if(set.add(word)==true) {
+//				  System.out.println("Yes, "+word+" is in the file");
+//			  	}}}
+//			
+//
+//			  else {
+//				  System.out.println("No, "+SearchWord+" is Not in the file");
+//				
+//			 }}}
+//           			  
+//
+//
+//   		
+//   	 
+//   				  
+//   			 
+//   			     catch(FileNotFoundException ex) {
+//   					  Logger.getLogger(Test.class.getName()).log(Level.SEVERE,null,ex);
+//   				     }catch(IOException ex) {
+//   					  Logger.getLogger(Test.class.getName()).log(Level.SEVERE,null,ex);
+//   				  }
+//   				  finally {
+//   					  fileR.close();
+//   				  }
+//     	
+//     	
+//        
+//		
+//		
+//}}
